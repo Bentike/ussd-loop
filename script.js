6e32db802eec597566125d320145e44c72adb1d0
bentike <ajadiyusuf73@gmail.com>
2023-08-24 12:52:47 +0100

    added defaultPassword variable for password check

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ let transfer = document.getElementById("transfer");
 let airtime = document.getElementById("airtime");
 let balance = document.getElementById("balance");
 
+const defaultPassword = "1234";
+
 // TRANSFER, this method handles USSD TRANSFER;
 const transferMoney = () => {
     let bank = prompt(
@@ -45,6 +47,7 @@ const buyAirtime = () => {
         alert(`You have succesfully purchased #${amount} ${network} airtime for ${number}`);
 }
 
+// checkBalance, this method is called when user clicked on Balance button; 
 const checkBalance = () => {
     alert("Your account balance is #10,000");
 }
@@ -53,44 +56,3 @@ transfer.addEventListener("click", transferMoney);
 airtime.addEventListener("click", buyAirtime);
 balance.addEventListener("click", checkBalance);
 
-// let ussdCode = prompt("Enter ussd Code");
-
-// let option = prompt("what do you want to do \n 1. Tranfers \n 2. Airtime \n 3. Balance");
-
-// // TRANSFER.
-// if(option === "1"){
-//     let bank = prompt("Choose Beneficiary's Bank \n 1. GTB \n 2. Second Bank \n 3. Access");
-//     let recieverAccountNumber = prompt("Enter Account Number");
-//     let amount = prompt("Enter Amount");
-//     alert("you are about to transfer " + "#" + amount + " to " + recieverAccountNumber);
-//     let pin = prompt("Enter your pin");
-//     alert(`You have successfully transfered #${amount} to ${recieverAccountNumber}`);
-// }else if(option === "2"){
-
-// }
-
-// // AIRTIME.
-// if(option === "2"){
-//     let network = prompt("Select Network \n 1. MTN \n 2. GLO \n 3. AIRTEL");
-//     switch(network){
-//         case "1":
-//             network = "MTN";
-//             break;
-//         case "2":
-//             network = "GLO";
-//             break;
-//         case "3":
-//             network = "AIRTEL";
-//             break;
-//         default:
-//             network = "MTN";
-
-//     }
-//     let number = prompt("Enter Receiver Mobile Number");
-//     let amount = prompt("Enter Card Amount");
-//     let confirmPin = prompt("Confirm your pin to continue");
-//     alert(`You have succesfully purchased #${amount} ${network} airtime for ${number}`);
-// }
-
-// // CHECK BALANCE
-// option === "3" ? alert("Your account balance is #10,000") : "";
