b47ec365391df1bf77ffcb203bb77ce3faf6bdb2
bentike <ajadiyusuf73@gmail.com>
2023-08-24 12:50:39 +0100

    implemented functionality to check balance

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ let transfer = document.getElementById("transfer");
 let airtime = document.getElementById("airtime");
 let balance = document.getElementById("balance");
 
-// TRANSFER.
-transferMoney = () => {
+// TRANSFER, this method handles USSD TRANSFER;
+const transferMoney = () => {
     let bank = prompt(
       "Choose Beneficiary's Bank \n 1. GTB \n 2. Second Bank \n 3. Access"
     );
@@ -22,9 +22,36 @@ transferMoney = () => {
     );
 };
 
+//BuyAIRTIME, this function handles airtime purchase;
+const buyAirtime = () => {
+    let network = prompt("Select Network \n 1. MTN \n 2. GLO \n 3. AIRTEL");
+        switch(network){
+            case "1":
+                network = "MTN";
+                break;
+            case "2":
+                network = "GLO";
+                break;
+            case "3":
+                network = "AIRTEL";
+                break;
+            default:
+                network = "MTN";
+    
+        }
+        let number = prompt("Enter Receiver Mobile Number");
+        let amount = prompt("Enter Card Amount");
+        let confirmPin = prompt("Confirm your pin to continue");
+        alert(`You have succesfully purchased #${amount} ${network} airtime for ${number}`);
+}
 
+const checkBalance = () => {
+    alert("Your account balance is #10,000");
+}
 
 transfer.addEventListener("click", transferMoney);
+airtime.addEventListener("click", buyAirtime);
+balance.addEventListener("click", checkBalance);
 
 // let ussdCode = prompt("Enter ussd Code");
 
