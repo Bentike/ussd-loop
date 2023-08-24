d08f2f4f0aff4cbe855f7b127d08ea8c52d2a44c
bentike <ajadiyusuf73@gmail.com>
2023-08-24 13:51:58 +0100

    added functionality to check password attempts

--- script.js
+++ script.js
@@ -6,53 +6,60 @@ const defaultPassword = "1234";
 
 // TRANSFER, this method handles USSD TRANSFER;
 const transferMoney = () => {
-    let bank = prompt(
-      "Choose Beneficiary's Bank \n 1. GTB \n 2. Second Bank \n 3. Access"
-    );
-    let recieverAccountNumber = prompt("Enter Account Number");
-    let amount = prompt("Enter Amount");
-    alert(
-      "you are about to transfer " +
-        "#" +
-        amount +
-        " to " +
-        recieverAccountNumber
-    );
-    let pin = prompt("Enter your pin");
+  let count = 3;
+  let bank = prompt(
+    "Choose Beneficiary's Bank \n 1. GTB \n 2. Second Bank \n 3. Access"
+  );
+  let recieverAccountNumber = prompt("Enter Account Number");
+  let amount = prompt("Enter Amount");
+  alert(
+    "you are about to transfer " + "#" + amount + " to " + recieverAccountNumber
+  );
+  let pin = prompt("Enter your pin");
+  if (pin === defaultPassword) {
     alert(
       `You have successfully transfered #${amount} to ${recieverAccountNumber}`
     );
+  }else{
+    while (count > 0 && pin !== defaultPassword) {
+        pin = prompt(
+          `Incorrect password, try again. you have ${count} attempt left`
+        );
+        count--;
+      }
+      alert("Too many attemps, Transaction cancelled");
+  }
 };
 
 //BuyAIRTIME, this function handles airtime purchase;
 const buyAirtime = () => {
-    let network = prompt("Select Network \n 1. MTN \n 2. GLO \n 3. AIRTEL");
-        switch(network){
-            case "1":
-                network = "MTN";
-                break;
-            case "2":
-                network = "GLO";
-                break;
-            case "3":
-                network = "AIRTEL";
-                break;
-            default:
-                network = "MTN";
-    
-        }
-        let number = prompt("Enter Receiver Mobile Number");
-        let amount = prompt("Enter Card Amount");
-        let confirmPin = prompt("Confirm your pin to continue");
-        alert(`You have succesfully purchased #${amount} ${network} airtime for ${number}`);
-}
+  let network = prompt("Select Network \n 1. MTN \n 2. GLO \n 3. AIRTEL");
+  switch (network) {
+    case "1":
+      network = "MTN";
+      break;
+    case "2":
+      network = "GLO";
+      break;
+    case "3":
+      network = "AIRTEL";
+      break;
+    default:
+      network = "MTN";
+  }
+  let number = prompt("Enter Receiver Mobile Number");
+  let amount = prompt("Enter Card Amount");
+  let confirmPin = prompt("Confirm your pin to continue");
+  alert(
+    `You have succesfully purchased #${amount} ${network} airtime for ${number}`
+  );
+};
 
-// checkBalance, this method is called when user clicked on Balance button; 
+// checkBalance, this method is called when user clicked on Balance button;
 const checkBalance = () => {
-    alert("Your account balance is #10,000");
-}
+  alert("Your account balance is #10,000");
+};
 
 transfer.addEventListener("click", transferMoney);
 airtime.addEventListener("click", buyAirtime);
 balance.addEventListener("click", checkBalance);
-
